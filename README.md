64b59bf8f372834993e72bd164450a7a22c0f537
Arthur Grand <artygrand@gmail.com>
2015-12-12 00:25:14 +0600

    First code

--- README.md
+++ README.md
@@ -1,2 +1,134 @@
-# shortcode
+# Shortcode standalone class
+
 Really simple shortcode parser without regex
+
+* without regex
+* nested shortcodes with unlimited level
+* with aliases
+* with the ability to show shortcode instead of compile it
+* with predefined arguments
+
+## Example
+
+```php
+$sample = '
+[shortcode]
+[short argument="value"]
+[shortcode active other="complex value"]
+[short]content[/short]
+[shortcode argument="value"]content[/shortcode]
+
+[span6]
+    [span6]
+        [span6   ]Lorem[/span6]
+        [span4]
+    [/span6]
+    [span4]
+        [span6 class="first" ]dolor[/span6]
+        [span6  class="last" ]cudere[/span6]
+    [/span4]
+[/span6]
+
+[span6]
+    @[span6]
+        [span6]Lorem[/span6]
+    [/span6]
+[/span6]
+';
+
+include 'path/to/Shortcode.php';
+
+$sc = artygrand\Shortcode::instance();
+
+$sc ->add('shortcode', 'shortcode')
+    ->withAlias('short')
+    ->add('span4', array(
+        'grid',
+        array(
+            'cols' => 4
+    )))
+    ->add('span6', array(
+        'grid',
+        array(
+            'cols' => 6
+    )))
+    ->withAlias('col6');
+
+echo $sample . "\n<hr>\n";
+echo $sc->compile($sample);
+
+// functions
+function shortcode($args, $content = null){
+    if (is_null($content)){
+        return print_r($args, true);
+    }
+    $args['content'] = $content;
+    return print_r($args, true);
+}
+
+function grid($args, $content = null){
+    $args = array_merge(array(
+        'cols' => 1,
+        'class' => '',
+    ), $args);
+
+    $class = $args['class'] ? ' class="' . $args['class'] . '"' : '';
+
+    if (is_null($content)){
+        return '<div style="width:' . ($args['cols']/0.12) .'%;"' . $class . '>NoContent</div>';
+    }
+    return '<div style="width:' . ($args['cols']/0.12) .'%;"' . $class . '>' . $content . '</div>';
+}
+```
+
+### End result will be like
+
+```
+Array
+(
+)
+
+Array
+(
+    [argument] => value
+)
+
+Array
+(
+    [active] => 1
+    [other] => complex value
+)
+
+Array
+(
+    [content] => content
+)
+
+Array
+(
+    [argument] => value
+    [content] => content
+)
+
+
+<div style="width:50%;">
+    <div style="width:50%;">
+        <div style="width:50%;">Lorem</div>
+        <div style="width:33.333333333333%;">NoContent</div>
+    </div>
+    <div style="width:33.333333333333%;">
+        <div style="width:50%;" class="first">dolor</div>
+        <div style="width:50%;" class="last">cudere</div>
+    </div>
+</div>
+
+<div style="width:50%;">
+    [span6]
+        <div style="width:50%;">Lorem</div>
+    [/span6]
+</div>
+```
+
+## License
+
+Released under the MIT License - see `LICENSE.txt` for details.
---
+++ src/Shortcode.php
@@ -0,0 +1,132 @@
+<?php
+namespace artygrand;
+
+class Shortcode{
+	private $codes;
+	private static $instance;
+
+	private function __construct(){}
+	private function __clone(){}
+	private function __wakeup(){}
+
+	public static function instance(){
+		if (empty(self::$instance)){
+			self::$instance = new self;
+		}
+		return self::$instance;
+	}
+
+	public function add($name, $value){
+		$this->codes[$name] = $value;
+		return self::$instance;
+	}
+
+	public function withAlias($alias){
+		end($this->codes);
+		$last = key($this->codes);
+		$this->codes[$alias] = $this->codes[$last];
+		return self::$instance;
+	}
+
+	public function remove($name){
+		unset($this->codes[$name]);
+		return self::$instance;
+	}
+
+	public function compile($text){
+		foreach ($this->codes as $code => $func){
+			$defaults = array();
+			if (is_array($func)){
+				$defaults = $func[1];
+				$func = $func[0];
+			}
+			
+			$open = '[' . $code;
+			$close = '[/' . $code . ']';
+			$data = array();
+			$matches = $this->matchBraces($text, $open, $close);
+
+			foreach($matches as $start => $len){
+				if ($len > 0){
+					$find = substr($text, $start, $len);
+					$attr_close = strpos($find, ']');
+					$content = substr($text, $start+1+$attr_close, $len-strlen($close)-1-$attr_close);
+				} else {
+					$attr_close = strpos($text, ']', $start)-$start;
+					$find = substr($text, $start, 1+$attr_close);
+					$content = null;
+				}
+
+				$attr_str = substr($find, strlen($open), $attr_close-strlen($open));
+				$attr = array_merge($defaults, $this->parseAttr($attr_str));
+				$replace = call_user_func_array($func, array($attr, $content));
+
+				$data[] = array(
+					$find,
+					$replace
+				);
+			}
+
+			foreach($data as $d){
+				$text = $this->replace($d[0], $d[1], $text);
+			}
+			$text = str_replace('@' . $open, $open, $text);
+		}
+
+		return $text;
+	}
+
+	private function parseAttr($attr_string){
+		$attr = array();
+		$pattern = '/([\w-]+)\s*(=\s*"([^"]*)")?/';
+		preg_match_all($pattern, $attr_string, $matches, PREG_SET_ORDER);
+		foreach ($matches as $match){
+			$name = strtolower($match[1]);
+			$value = isset($match[3]) ? $match[3] : true;
+			$attr[$name] = $value;
+		}
+		return $attr;
+	}
+
+	private function replace($search, $replace, $text){
+		$pos = strpos($text, $search); 
+		return $pos !== false ? substr_replace($text, $replace, $pos, strlen($search)) : $text; 
+	}
+
+	private function matchBraces($str, $open_tag = '{{', $close_tag = '}}'){
+		$stack = $result = array();
+		$len_open = strlen($open_tag);
+		$len_close = strlen($close_tag);
+		$pos = -1;
+		$end = strlen($str) + 1;
+
+		while (true){
+			$p1 = strpos($str, $open_tag, $pos + 1);
+			$p2 = strpos($str, $close_tag, $pos + 1);
+			$pos = min(($p1 === false) ? $end : $p1, ($p2 === false) ? $end : $p2);
+			if ($pos == $end){
+				break;
+			}
+			if (substr($str, $pos, $len_open) == $open_tag){
+				$stack[] = $pos;
+			} elseif (substr($str, $pos, $len_close) == $close_tag){
+				if (count($stack)){
+					$start = array_pop($stack);
+					if ($str{$start-1} != '@'){
+						$result[$start] = $pos + $len_close - $start;
+					}
+				}
+			}
+		};
+
+		// self-closing shortcodes
+		foreach ($stack as $start){
+			if ($str{$start-1} != '@'){
+				$result[$start] = 0;
+			}
+		}
+
+		ksort($result);
+		return $result;
+	}
+}
